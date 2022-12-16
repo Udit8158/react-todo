b38304623cd57c58ddf13a3118560037d534aba8
Udit Kundu <kunduudit8158@gmail.com>
2022-12-16 13:16:51 +0530

    done version 1

--- src/components/UI/Banner.jsx
+++ src/components/UI/Banner.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Banner({ message, color }) {
   return (
     <div
-      className={`absolute w-screen left-0 top-0 p-6 text-white text-2xl bg-${color}-700`}
+      className={`absolute w-screen left-0 top-0 p-6 text-white text-2xl ${
+        color === "red" ? "error" : "success"
+      }`}
     >
       {message}
     </div>
